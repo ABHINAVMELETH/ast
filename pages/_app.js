960450b3fcf563972bf71e03fe02de10258df4ed
ABHINAVMELETH <melethabhinav@gmail.com>
2024-10-06 21:37:00 +0530

    added backend

--- pages/_app.js
+++
@@ -1,7 +0,0 @@
-import '@/styles/globals.css'
-import '@fortawesome/fontawesome-free/css/all.min.css';
-
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
